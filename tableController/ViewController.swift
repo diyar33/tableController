cb05a07648c2a95b710a1535eb89764dd2708405
Diyar Aydın <diyaraydin33@gmail.com>
2020-04-17 20:55:12 +0300

    ikinci

--- tableController/ViewController.swift
+++ tableController/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let ilkkontroll=0
-        print(ilkkontroll)
+let ikinci=1
     }
 
 
