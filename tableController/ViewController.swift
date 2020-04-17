75b25ce3352b5a57577833abeb82a84d80bd8e81
Diyar Aydın <diyaraydin33@gmail.com>
2020-04-17 20:47:27 +0300

    Initial Commit

---
+++ tableController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableController
+//
+//  Created by Ahmet Tarık Sürücü on 17.04.2020.
+//  Copyright © 2020 Diyar Aydın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
