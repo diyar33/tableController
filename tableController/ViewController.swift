e0a7825259df38d06e57b8989c3e4a983995beb7
Diyar Aydın <diyaraydin33@gmail.com>
2020-04-17 20:53:27 +0300

    table ilk kodu

--- tableController/ViewController.swift
+++ tableController/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let ilkkontroll=0
+        print(ilkkontroll)
     }
 
 
